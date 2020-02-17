f9b04c70144ec31b0bac3dc1a27991cdb161c287
fallndiack <fallndiack1@gmail.com>
2020-02-17 18:30:53 +0100

    création des prepieres pages home et archive et leur route

---
+++ AgenceImmobiliere/src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index(): Response
+    {
+
+        return $this->render('pages/home.html.twig');
+    }
+}
