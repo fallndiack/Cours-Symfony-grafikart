990486a5be2e5b6bf0073959e6aa844567ebd803
fallndiack <fallndiack1@gmail.com>
2020-02-20 23:16:05 +0100

    feat: ajout de l'authentification avec security

--- AgenceImmobiliere/src/Entity/Property.php
+++ AgenceImmobiliere/src/Entity/Property.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraint as Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
